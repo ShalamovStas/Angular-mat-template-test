e14cc9e635aefe927780470fcfa80299778628d5
stanislav.shalamov@ssa.group <shalamov.stas1992@gmail.com>
2020-08-29 01:36:51 +0200

    init app

---
+++ src/app/material-component/custom-questions/custom-questions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-questions',
+  templateUrl: './custom-questions.component.html',
+  styleUrls: ['./custom-questions.component.scss']
+})
+export class CustomQuestionsComponent {
+  constructor() { }
+}
